50a0f104c0f2c42a5dc3c57e71fe30f46d2d007e
Marcos Garcia de Lima <marckfree@gmail.com>
2013-09-23 14:05:47 -0300

    Implementa página para exibição de vídeos por categoria

--- wordpress/wp-content/themes/tv-maua-e-regiao/category.php
+++ wordpress/wp-content/themes/tv-maua-e-regiao/category.php
@@ -2,56 +2,60 @@
 <?php get_header(); ?>
 
 <div class='conteudo'>
-  <div class='player'>
+  <div class="nome-categoria">
+    <h2><?php echo $category[0]->name; ?></h2>
+  </div>
+  <div class='player individual'>
     <div class='video'>
       <a href='http://localhost/TVMauaERegiao/wordpress/wp-content/uploads/2013/09/play.flv' id='flv-player'></a>
       <!-- <a href='http://marcker.net/tv-wp/wp-content/uploads/2013/09/play.flv' id='flv-player'></a> -->
     </div>
   </div>
-  <div class='publicidade'>
-    <?php query_posts('order=ASC&posts_per_page=1&tag=publicidade-home'); ?>
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <a href='<?php echo get_post_meta($post->ID, 'Link Publicidade 180 x 450', true); ?>' target='_blank'>
-        <img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, 'Publicidade 180 x 450', true); ?>&amp;w=180&amp;450" alt="">
-      </a>
-    <?php endwhile; else: ?>
-      <img src='<?php bloginfo("template_url"); ?>/imagens/sem-publicidade.jpg' alt=''>
-    <?php endif; ?>
-  </div>
   <div class='informacoes-anunciante'>
     <div class='nome-anunciante'></div>
-    <div class='categoria'></div>
   </div>
   <div class='botao-mais-informacoes'>
     <input type='button' value='Mais Informações'>
   </div>
+  <div class='sombra'></div>
 </div>
 <div class='lista-de-videos'>
-  <div class='controles'>
-    <input class='anterior' title='Anterior' type='button' value=' '>
-    <input class='proximo' title='Próximo' type='button' value=' '>
-  </div>
-  <div class='clips'>
+  <div class='clips clips-categoria'>
+    <div class="cabecalho-lista-de-videos">
+      <h2>Anunciantes</h2>
+    </div>
     <ul>
       <?php query_posts("order=ASC&posts_per_page=20&tag=videos&category_name=" . $category[0]->slug); ?>
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <li>
-          <?php $categoria = get_the_category(); ?>
-          <a data-categoria='<?php echo $categoria[0]->cat_name; ?>' href='<?php echo get_post_meta($post->ID, "VÍDEO", true); ?>' title='<?php the_title(); ?>'>
-            <img alt='' src='<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, "Miniatura VÍDEO", true); ?>&amp;w=220&amp;h=180'>
-            <p><?php the_title(); ?></p>
+          <?php $category = get_the_category(); ?>
+          <a data-categoria='<?php echo $category[0]->cat_name; ?>' href='<?php echo get_post_meta($post->ID, "VÍDEO", true); ?>' title='<?php the_title(); ?>'>
+            <img alt='' src='<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, "Miniatura VÍDEO", true); ?>&amp;w=200&amp;h=160'>
           </a>
         </li>
       <?php endwhile; else: ?>
       <?php endif; ?>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo bloginfo('template_url'); ?>/imagens/miniaturas/10deep.jpg&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, "Miniatura VÍDEO", true); ?>&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, "Miniatura VÍDEO", true); ?>&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, "Miniatura VÍDEO", true); ?>&amp;w=200&amp;h=160'" alt=""></a></li>
+      <li><a href=""><img src="<?php bloginfo("template_url"); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID, "Miniatura VÍDEO", true); ?>&amp;w=200&amp;h=160'" alt=""></a></li>
     </ul>
   </div>
 </div>
-<div class='box'>
-  <div class='elementos'>
-    <p>Aqui serão publicados novos conteúdos lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, reprehenderit laboriosam nobis dolor minima eaque obcaecati! Tenetur, non, eum perspiciatis alias deleniti illo quo pariatur. Nesciunt, eos velit optio laborum!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, consectetur alias soluta assumenda molestiae facilis at iusto aut harum porro saepe architecto quidem omnis accusantium eius neque dolores illo adipisci! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, pariatur voluptatibus blanditiis dicta mollitia nesciunt vero modi cupiditate consectetur dignissimos ea nemo consequatur ad animi natus deleniti corrupti quo explicabo!</p>
-  </div>
-</div>
-
 <?php get_footer(); ?>

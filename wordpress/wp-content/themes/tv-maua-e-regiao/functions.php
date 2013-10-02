2bc7d24f0120306192360e2810bce40dbf7f38ca
Marcos Garcia de Lima <marckfree@gmail.com>
2013-10-02 14:47:50 -0300

    Adiciona arquivo functions.php

---
+++ wordpress/wp-content/themes/tv-maua-e-regiao/functions.php
@@ -0,0 +1,48 @@
+<?php
+function definirTitulo() {
+  $arrCategoria = get_the_category();
+
+  function verificaQTDVideos($arrCategoria) {
+    $QTDVideos = $arrCategoria[0]->count;
+    return $QTDVideos > 1 ? $QTDVideos . " vídeos" : $QTDVideos . " vídeo";
+  }
+
+  $categoria = $arrCategoria[0]->cat_name . " (" . verificaQTDVideos($arrCategoria) . ")";
+
+  $nome = get_bloginfo('name');
+  $tituloPadrao = "&#x25B6; " . $nome;
+  $titulo = get_the_title();
+  $busca = get_search_query();
+
+  if (is_home()) {
+    $tituloWebsite = $tituloPadrao;
+  } elseif (is_page()) {
+    $tituloWebsite = $tituloPadrao . " | " . $titulo;
+  } elseif (is_category()) {
+    $tituloWebsite = $tituloPadrao . " | " . $categoria;
+  } elseif (is_search()) {
+    $tituloWebsite = $tituloPadrao . " | Busca por " . $busca;
+  } elseif (is_404()) {
+    $tituloWebsite = $tituloPadrao . " | Página não encontrada";
+  }
+
+  return $tituloWebsite;
+}
+
+function categoriasSemTitle() {
+  $categories = wp_list_categories('show_count=1&echo=0&orderby=name&title_li&exclude=6');
+  $categories = preg_replace('/title=\"(.*?)\"/', '', $categories);
+  $categories = str_replace("(", "<i>", $categories);
+  $categories = str_replace(")", "</i>  ", $categories);
+  return $categories;
+}
+
+function adicionarClasse() {
+  if (is_home()) {
+    $classe = "home";
+  } else {
+    $classe = "";
+  }
+  return $classe;
+}
+?>

d4bd77764c5c5f949ad149cff46654b959a6e4a3
Marcos Garcia de Lima <marckfree@gmail.com>
2013-10-15 09:43:14 -0300

    Adiciona texto a página 'Anuncie'

--- wordpress/wp-content/themes/tv-maua-e-regiao/anuncie.php
+++ wordpress/wp-content/themes/tv-maua-e-regiao/anuncie.php
@@ -6,17 +6,17 @@
     <div class="elementos">
       <div class="texto">
         <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-          <li>A, unde reiciendis dolorum ipsum esse nobis perferendis quidem 
-              facilis? Nemo obcaecati odio ipsam natus rem expedita esse ut 
-              illo eius perspiciatis!</li>
-          <li>Lorem ipsum dolor sit amet</li>
-          <li>Quas ullam adipisci autem enim minus pariatur accusantium nesciunt odit sint.</li>
-          <li>Minus pariatur accusantium nesciunt odit sint.</li>
-          <li>Accusantium nesciunt odit sint.</li>
-          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, odit, consequatur, molestias dolorem eos consequuntur officia ad debitis accusantium aut recusandae temporibus animi nam eius reprehenderit nostrum excepturi dolorum atque.</li>
+          <li>Canal 24 horas no ar, voltado para vendas de produtos e serviços variados, promocional e institucional.</li>
+          <li>Comercial realizado no local do anunciante, gerando maior visibilidade dos produtos, fabricação e/ou serviço, criando ainda mais credibilidade junto ao consumidor.</li>
+          <li>Direcionado ao público alvo interessado em comprar.</li>
+          <li>Fácil acesso aos vídeos através de busca automática no site.</li>
+          <li>Dinamismo e flexibilidade na locução e apresentação.</li>
+          <li>Ampla divulgação interna e externa via "fanpage", portal, jornal, revista, mailings e outros.</li>
+          <li>Mensuração de resultados e quantidade de acessos e visualização aos anunciantes.</li>
+          <li>Renovação dos comercias no prazo solicitado.</li>
         </ul>
-        <p class="margem-abaixo"><span>Ligue 11 4513 1591 e solicite a visita de um representante.</span></p>
+
+        <p class="margem-abaixo"><span>Grupo Mauá e Região de Comunicação - Ligue 11 4513 1591 e solicite a visita de um representante.</span></p>
 
         <div class="sombra"></div>
       </div>
